3f7ecf92f0d557fd69b0e6d39d63a41fa8e5e1b9
Antonio Sánchez <anventech@gmail.com>
2025-05-10 01:40:30 -0400

    Success toast

---
+++ src/renderer/src/components/SuccessToast.jsx
@@ -0,0 +1,17 @@
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import IconAndText from "./IconAndText";
+
+function SuccessToast() {
+  return (
+    <div className="flex flex-col gap-1" style={{
+      fontFamily: "Montserrat Variable"
+    }}>
+      <div className="flex items-center text-lime-400">
+        <IconAndText icon={faCheckCircle} />
+        <span className="text-md">Your download is ready!</span>
+      </div>
+    </div>
+  );
+}
+
+export default SuccessToast;
